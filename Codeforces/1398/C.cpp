cdd6ad83f84d173c887a256103b89b87cf4131b4
kORNkin <yeemee.0012za@email.com>
2026-08-25 15:20:52 +0800

    add cf1398 c

--- Codeforces/1398/C.cpp
+++ Codeforces/1398/C.cpp
@@ -7,22 +7,18 @@ int main(){
     int t; cin >> t;
 
     while(t--){
-        int n, cnt = 0; cin >> n;
-        int prefix[n + 1];
-        memset(prefix, 0, sizeof(prefix));
+        long long n, ans = 0, sum = 0; cin >> n;
         
         string arr; cin >> arr;
-        for(int i = 1; i <= n; i++){
-            int j = arr[i - 1] - '0';
-            prefix[i] = prefix[i - 1] + j; 
+        unordered_map<int, long long> mp;
 
-            for(int k = i; k >= j && k > 0; k--){
-                int sum = prefix[i] - prefix[i - k];
-                if(sum == k) cnt++;
-                else if(sum > k) break;
-            }
+        mp[0]++;
+        for(int i = 0; i < n; i++) {
+            sum += arr[i] - '0';
+            ans += mp[sum - i - 1];
+            mp[sum - i - 1]++;
         }
-        cout << cnt << '\n';
+        cout << ans << '\n';
     }
 
     return 0;
