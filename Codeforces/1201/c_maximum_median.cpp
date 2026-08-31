32a9a3d7c87b5872bfe2387c18c4640b04bebc7c
kORNkin <yeemee.0012za@gmail.com>
2026-09-01 00:59:36 +0800

    Add cf 1201 c

---
+++ Codeforces/1201/c_maximum_median.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, k;
+
+int solve(int l,int r, int arr[], function<bool(int, int[])> f){
+    while(l < r){
+        int m = l + ((r - l + 1)>>1);
+        if(f(m, arr)) l = m;
+        else r = m - 1;
+    }
+
+    return l;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+
+    cin >> n >> k;
+    int arr[n];
+    for(int &a : arr) cin >> a;
+
+    sort(arr, arr + n);
+
+    cout << solve(arr[((n+1)>>1) - 1], 2 * 10e5, arr, [](int m, int arr[]) -> bool{
+        int op_use = 0;
+
+        for(int i = ((n+1)>>1) - 1; i < n; i++){
+            op_use += max(0, m - arr[i]);
+        }
+        return op_use <= k;
+    });
+
+    return 0;
+}
